1cbb670e061f7017bbb9df9a83cad85112eeb396
Lazy_boy <147528711+Learn-until-die@users.noreply.github.com>
2024-03-20 11:23:11 +0600

    Add files via upload
    small game

---
+++ game.js
@@ -0,0 +1,8 @@
+let name = prompt("Enter your name here");
+let Name = name;
+document.write("Congratulations " + Name  );
+let num = 89;
+let game = prompt( Name + " Guess the number");
+while(game != num){
+  game = prompt( Name + " Try again");
+}
